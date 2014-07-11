fa0dc7920127f462a82390815691e8d7c2f35ffd
Stoyan <s.dimitrov19@gmail.com>
2014-07-11 12:19:12 -0700

    updated the testing

--- index.js
+++ index.js
@@ -2,3 +2,4 @@
  * Created by stoyan on 7/10/14.
  */
 module.exports = require("./lib/bitemit");
+
--- test/index.js
+++ test/index.js
@@ -1,2 +1,21 @@
-var should = require('chai').should(),
+var chai = require('chai'),
+  assert = chai.assert,
+  expect = chai.expect,
+  should = chai.should(),
     bitemit = require('../index');
+
+
+suite('Assertions', function(){
+  test('should pass on truthiness', function(){
+    assert.ok(true);
+  });
+  test('should fail on falsiness', function(){
+    assert.ok(false);
+  });
+  describe('aa', function(){
+    it('returns adfafda ', function(){
+      bitemit.a.should.be.a('number');
+    });
+  });
+
+});
